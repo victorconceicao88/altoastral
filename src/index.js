f7cb5ec80f63788d85686683791e9ae07ad5b02a
Victor conceicao <victor.engenhariasoftware@gmail.com>
2025-06-02 17:16:58 +0100

    service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,8 +15,6 @@ root.render(
   </React.StrictMode>
 );
 
-// Registra o Service Worker para PWA
-serviceWorkerRegistration.register();
 
 // Se quiser medir performance
 reportWebVitals();
