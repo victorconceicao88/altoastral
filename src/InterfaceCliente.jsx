a15dc8e4ef839a4ae11dac03b323211b426d541b
victor.conceicao <victor.engenhariasoftware@gmail.com>
2025-06-03 17:02:19 +0100

    elint

--- src/InterfaceCliente.jsx
+++ src/InterfaceCliente.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useCallback } from 'react';
 import { ref, push, update, database, loginAnonimo } from './firebase';
 import { getDatabase, set, onValue } from 'firebase/database';
